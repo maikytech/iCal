093a03b7a5506cf0a20dae65cc84b0ca14f430ea
maikytech <maikytech1307@gmail.com>
2021-09-14 21:33:17 -0500

    Initial window setup

--- iCal/AppDelegate.swift
+++ iCal/AppDelegate.swift
@@ -13,8 +13,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Setup
+        setupView()
+
         return true
+    }
+    
+    //MARK: - Private functions
+    func setupView() {
+        
+        //UIScreen.main.bounds = entire screen.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
     }
 
     
---
+++ iCal/Controller/Home/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  iCal
+//
+//  Created by Maiqui Cedeño on 13/09/21.
+//
+
+import UIKit
+
+//Without extensions
+final class HomeViewController: UIViewController {
+    
+    //MARK: - Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
